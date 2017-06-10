7d8bc2a8f7b1ab7a36bb6ab3ccbed5b3b33a307e
easton <eastonqiu@163.com>
2017-06-10 20:13:18 +0800

    add Business Models

---
+++ app/Http/Controllers/Web/Admin/SlotController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Web\Admin;
+
+use Illuminate\Http\Request;
+
+class SlotController extends Controller
+{
+    //
+}
---
+++ app/Models/Station.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Station extends Model
+{
+    use SoftDeletes;
+	
+    protected $guarded = [
+        'id', 'created_at', 'updated_at'
+    ];
+}
